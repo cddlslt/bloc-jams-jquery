fa920d81517c2dd53759bae4017ec5c3baeb592a
cddlslt <oatesmi.74@gmail.com>
2017-09-18 14:26:30 -0500

    checkpoint 2.13 added player bar functionality

---
+++ scripts/player-bar.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+  $('button#play-pause').click(function() {
+    player.playPause();
+    $(this).attr('playState', player.playState);
+  });
+
+  $('button#next').click( function() {
+    if (player.playState !== 'playing') {return; }
+
+    const CURRENT_SONG_INDEX = album.songs.indexOf(player.currentlyPlaying);
+    const NEXT_SONG_INDEX = CURRENT_SONG_INDEX + 1;
+    if (NEXT_SONG_INDEX >= album.songs.length) {return; }
+    
+    const NEXT_SONG = album.songs[NEXT_SONG_INDEX];
+    player.playPause(NEXT_SONG);
+  });
+});
